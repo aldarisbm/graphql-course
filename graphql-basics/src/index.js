fbda5f16d0bf040f4e65d828d7677507b8cfb3f1
Jose Berrio <aldarisbm@gmail.com>
2021-07-15 12:26:33 -0400

    playing with Scalar types, and typeDefs

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,56 @@
-console.log("Hello GraphQL")
+import { GraphQLServer } from 'graphql-yoga'
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type definitions (schema)
+
+const typeDefs = `
+    type Query {
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+    
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`
+// Resolvers
+
+const resolvers = {
+    Query: {
+        me() {
+            return {
+                id: 123,
+                name: "Mike",
+                email: "mike@example.com"
+            }
+        },
+        post() {
+            return {
+                id: 321,
+                title: "GraphQL rocks",
+                body: "I love working on GraphQL",
+                published: false
+            }
+        }
+    } 
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+
+server.start(() => {
+    console.log("server is up")
+})
